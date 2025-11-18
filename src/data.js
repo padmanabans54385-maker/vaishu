7ff30dc216d822176414fa4ed67b1786764c55cd
padmanabans54385-maker <padmanabans54385@gmail.com>
2025-11-18 13:40:58 +0530

    Add files via upload

---
+++ src/data.js
@@ -0,0 +1,92 @@
+// ==========================
+// 1. Intro Screen
+// ==========================
+export const firstHeading = "A Cutiepie was born today, 21 years ago!"
+// Main heading text - Change this text as you like
+
+export const firstSubtext = "Yes, it’s YOU! A little surprise awaits..."
+// Subtext under heading (optional) - Change, leave blank or remove.
+
+
+// ==========================
+// 2. Cake Screen
+// ==========================
+// Add recipient's name here.
+export const NAME = "Vaishu💙"
+
+
+// ==========================
+// 3. Photo Screen
+// ==========================
+export const photoScreenHeading = "Some Sweet Moments With You"
+// Heading above photos - Change this text as you like
+
+// Add your photos inside "public/images" folder
+// Example: { "/images/1.jpeg" }, Match the format and add more as needed
+export const photos = [
+    "/images/1.jpg",
+    "/images/2.jpg",
+    "/images/3.jpg",
+    "/images/4.jpg",
+    "/images/5.jpg",
+    "/images/6.jpg",
+    "/images/7.jpg",
+    "/images/8.jpg",
+    "/images/9.jpg",
+    "/images/10.jpg",
+    "/images/11.jpg"
+]
+// Add your videos inside "public/videos" folder
+// Example format: "/videos/video1.mp4"
+
+export const videos = [
+    "/videos/1.mp4",
+    "/videos/2.mp4",
+    "/videos/3.mp4",
+    "/videos/4.mp4",
+    "/videos/19.mp4",
+    "/videos/6.mp4",
+    "/videos/7.mp4",
+    "/videos/8.mp4",
+    "/videos/9.mp4",
+    "/videos/10.mp4",
+    "/videos/11.mp4",
+    "/videos/21.mp4",
+    "/videos/13.mp4",
+    "/videos/14.mp4",
+    "/videos/15.mp4",
+    "/videos/16.mp4",
+    "/videos/17.mp4",
+    "/videos/18.mp4",
+    "/videos/22.mp4",
+    "/videos/20.mp4"
+]
+
+
+
+// ==========================
+// 4. Message Screen
+// ==========================
+export const messageScreenHeading = "A Special Message"
+// Heading above card - Change this text as you like
+
+export const specialMessage = `Happy Birthday, Vaishu💙! You deserve all the happiness, love and smiles in the world today and always. Nee romba nalla ponnu eruma😇 ennakaga eppavum ennkuda irunthuruka iruka inium irupa💗 nammakula eppdi bonding achunu theriyala but nee ennaku keadachthuku I feel very lucky 😇💯. Gallery pathurupa onnkuda time spend pannathu ennaku romba happy time I want this bond always in my life. I hope your day filled with laughter, surprises, and moments that make your heart 😊. Wishing you endless happiness, success,and all the sweet things life has to offer 💝.`
+// Main special message (you can edit freely)
+
+
+// ==========================
+// 4. Final Screen
+// ==========================
+export const finalScreenHeading = "One Last Thing..."
+// Heading above final surprise - Change this text as you like
+
+export const overlayText = "Lots of love for you 💙"
+// First text inside the overlay card - Change this text as you like
+
+export const overlayMessage = "Once again, Happy Birthday Vaishu Eruma! Hope you loved your surprise."
+
+// ==========================
+// Background Music
+// ==========================
+// Place your .mp3 file inside "public/audio" and update path below
+export const backgroundMusic = "./audio/bg.mp3"
